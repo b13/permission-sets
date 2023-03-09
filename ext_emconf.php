fc49fda517c0ee73731eebe83f31a91d3d06a8cf
Kai Ole Hartwig <o.hartwig@moselwal.de>
2023-03-09 17:12:49 +0100

    Support for v11 and v12

--- ext_emconf.php
+++ ext_emconf.php
@@ -12,7 +12,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '0.1.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '12.0.0-12.9.99',
+            'typo3' => '11.5.0-12.9.99',
         ],
         'conflicts' => [],
         'suggests' => [],
